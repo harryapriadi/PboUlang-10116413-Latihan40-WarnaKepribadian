defcc1ee34c96a4ca18dc2dd57ae9f801bd569a8
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-17 14:23:36 +0700

    PboUlang-10116413-Latihan40-WarnaKepribadian

---
+++ src/pboulang/pkg10116413/latihan40/warnakepribadian/PboUlang10116413Latihan40WarnaKepribadian.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PboUlang10116413Latihan40WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
